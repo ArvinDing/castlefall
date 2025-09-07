13a0e686574b4fba967ed55e7e255418d9540e60
Suhas Nagar <suhas.t.nagar@gmail.com>
2025-09-07 19:05:02 -0400

    Backend updates

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,24 +4,94 @@ use axum::{
     routing::get,
     Router,
 };
-use futures::stream::{Stream, StreamExt};
-use serde::Deserialize;
+use futures::stream::Stream;
+use serde::{Deserialize, Serialize};
 use std::{
-    collections::HashMap,
+    collections::{HashMap},
     net::SocketAddr,
     sync::{Arc, Mutex},
 };
 use tokio::sync::broadcast;
 
-#[derive(Debug)]
+#[derive(Clone)]
 struct Room {
     names: Vec<String>,
+    round_num: i32,
+    words: Vec<String>,
+    assignments: Assignments,
+    tx: broadcast::Sender<String>,
 }
+
+impl Room {
+    fn new() -> Self {
+        let (tx, _rx) = broadcast::channel(100);
+        Self {
+            names: vec![],
+            round_num: 0,
+            words: vec![],
+            assignments: Assignments::new(),
+            tx,
+        }
+    }
+}
+
+#[derive(Serialize, Debug, Clone)]
+struct Assignments {
+    pairs: Vec<(String, String)>, // (player, word)
+}
+
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+impl Assignments {
+    fn new() -> Self {
+        Self { pairs: Vec::new() }
+    }
+
+    fn assign_random_words(&mut self, words: &[String], players: &[String]) {
+        if words.len() < 2 {
+            panic!("Need at least 2 words to assign players");
+        }
+
+        let mut rng = thread_rng();
+
+        // Pick two random words
+        let chosen_words: Vec<&String> = words.choose_multiple(&mut rng, 2).collect();
+        let word1 = chosen_words[0].clone();
+        let word2 = chosen_words[1].clone();
+
+        // Shuffle players
+        let mut shuffled_players = players.to_vec();
+        shuffled_players.shuffle(&mut rng);
+
+        let half = (shuffled_players.len() + 1) / 2; // round up if odd number
+
+        // Assign first half to word1, second half to word2
+        for player in shuffled_players.iter().take(half) {
+            self.pairs.push((player.clone(), word1.clone()));
+        }
+        for player in shuffled_players.iter().skip(half) {
+            self.pairs.push((player.clone(), word2.clone()));
+        }
+    }
+
+    fn remove_player(&mut self, player: &str) {
+        self.pairs.retain(|(p, _)| p != player);
+    }
+}
+
 
 #[derive(Clone)]
 struct AppState {
-    rooms: Arc<Mutex<HashMap<String, Room>>>,
-    tx: broadcast::Sender<String>, // broadcaster
+    rooms: Arc<Mutex<HashMap<String, Room>>>, // global mutex
+}
+
+#[derive(Serialize)]
+struct RoomStateView {
+    round_num: i32,
+    names: Vec<String>,
+    word_list: Vec<String>,
+    assignments: Assignments,
 }
 
 #[derive(Deserialize)]
@@ -30,52 +100,105 @@ struct JoinParams {
     room: String,
 }
 
-#[tokio::main]
-async fn main() {
-    let (tx, _rx) = broadcast::channel(100);
-
-    let state = AppState {
-        rooms: Arc::new(Mutex::new(HashMap::new())),
-        tx,
-    };
-
-    let app = Router::new()
-        .route("/join", get(join_handler))
-        .route("/events", get(events_handler))
-        .with_state(state);
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Server running at http://{}/", addr);
-
-    axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app)
-        .await
-        .unwrap();
+#[derive(Deserialize)]
+struct KickParams {
+    name: String,
+    room: String,
 }
+
+#[derive(Deserialize)]
+struct NextRoundParams {
+    room: String,
+}
+
+use axum::Json;
+
+// ========================= Handlers =========================
 
 async fn join_handler(
     State(state): State<AppState>,
     Query(params): Query<JoinParams>,
-) -> impl IntoResponse {
+) -> Json<RoomStateView> {
     let mut rooms = state.rooms.lock().unwrap();
+    let room = rooms.entry(params.room.clone()).or_insert_with(Room::new);
 
-    let room = rooms.entry(params.room.clone()).or_insert(Room { names: vec![] });
     if !room.names.contains(&params.name) {
         room.names.push(params.name.clone());
     }
 
-    let msg = format!("{} joined room {}", params.name, params.room);
-    println!("{}", msg);
+    let _ = room.tx.send(format!("JOIN, {}, {}", params.name, params.room));
 
-    // Broadcast to all listening clients
-    let _ = state.tx.send(msg.clone());
+    let view = RoomStateView {
+        round_num: room.round_num,
+        names: room.names.clone(),
+        word_list: room.words.clone(),
+        assignments: room.assignments.clone(),
+    };
 
-    format!("Room {} now has members: {:?}", params.room, room.names)
+    Json(view)
+}
+
+async fn kick_handler(
+    State(state): State<AppState>,
+    Query(params): Query<KickParams>,
+) -> impl IntoResponse {
+    let mut rooms = state.rooms.lock().unwrap();
+
+    if let Some(room) = rooms.get_mut(&params.room) {
+        if let Some(pos) = room.names.iter().position(|n| n == &params.name) {
+            room.names.remove(pos);
+            room.assignments.remove_player(&params.name);
+
+            let _ = room.tx.send(format!("KICK, {}, {}", params.name, params.room));
+
+            return format!(
+                "{} removed. Room {} now has members: {:?}",
+                params.name, params.room, room.names
+            );
+        } else {
+            return format!("{} is not in room {}", params.name, params.room);
+        }
+    }
+
+    format!("Room {} does not exist", params.room)
+}
+
+async fn start_round_handler(
+    State(state): State<AppState>,
+    Query(params): Query<NextRoundParams>,
+) -> impl IntoResponse {
+    let mut rooms = state.rooms.lock().unwrap();
+
+    if let Some(room) = rooms.get_mut(&params.room) {
+        room.round_num += 1;
+
+        room.words = vec![
+            "apple", "banana", "cherry", "date", "elderberry", "fig", "grape",
+            "honeydew", "kiwi", "lemon", "mango", "nectarine", "orange", "papaya",
+            "quince", "raspberry", "strawberry", "tangerine"
+        ]
+        .into_iter()
+        .map(String::from)
+        .collect();
+
+        room.assignments = Assignments::new();
+        room.assignments.assign_random_words(&room.words, &room.names);
+        let _ = room.tx.send(format!("ROUND_START, {}", params.room));
+
+        format!("Round started in room {}! Round number: {} Assignments: {:?}", params.room, room.round_num, room.assignments)
+    } else {
+        format!("Room {} does not exist", params.room)
+    }
 }
 
 async fn events_handler(
     State(state): State<AppState>,
+    Query(params): Query<JoinParams>,
 ) -> Sse<impl Stream<Item = Result<Event, axum::Error>>> {
-    let mut rx = state.tx.subscribe();
+    let rooms = state.rooms.lock().unwrap();
+    let room = rooms.get(&params.room).expect("Room must exist");
+
+    let mut rx = room.tx.subscribe();
 
     let stream = async_stream::stream! {
         while let Ok(msg) = rx.recv().await {
@@ -86,8 +209,25 @@ async fn events_handler(
     Sse::new(stream)
 }
 
-// player join
-// round start
-// start the shot clock
-// kick player
-// reset score for everyone
+// ========================= Main =========================
+
+#[tokio::main]
+async fn main() {
+    let state = AppState {
+        rooms: Arc::new(Mutex::new(HashMap::new())),
+    };
+
+    let app = Router::new()
+        .route("/join", get(join_handler))
+        .route("/kick", get(kick_handler))
+        .route("/events", get(events_handler))
+        .route("/next_round", get(start_round_handler))
+        .with_state(state);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Server running at http://{}/", addr);
+
+    axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app)
+        .await
+        .unwrap();
+}
